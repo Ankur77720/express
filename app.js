b0fa2363501d107d23b6b4880c649c0777d1801b
Ankur77720 <prajapatiankur8349@gmail.com>
2024-09-18 17:15:17 +0530

    notes done

--- app.js
+++ app.js
@@ -1,73 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
+const notes = []
 
-
-app.use(function (req, res, next) {
-
-    req.data = 'this is a data'
-    console.log('this is a global middleware')
-
-    next()
+app.get('/', (req, res) => {
+    res.render('index', { notes: notes });
 })
 
-/* 
+app.post('/create-note', (req, res) => {
 
-global middleware
-route specific middleware
-
-*/
-
-
-
-app.get('/', function (req, res, next) {
-
-    console.log('this is a route "/" specific middleware')
-    next()
-
-}, function (req, res) {
-
-
-    console.log(req.data)
-
-
-    res.render('index', {
-        username: "john",
-        age: 20,
-    })
+    notes.push(req.body)
+    console.log(notes)
+    res.redirect('/')
 })
 
 
-app.get('/about',
-
-    function (req, res, next) {
-
-        console.log('this is a route "/about" specific middleware')
-        next()
-    },
-
-    function (req, res) {
-        res.send('about')
-    })
-
-app.get('/profile', function (req, res) {
-
-    /* 
-    req.query
-    req.params
-    req.body
-    */
-
-    res.json({
-        name: 'John',
-        age: 20,
-        email: 'john@gmail.com',
-    })
-})
-
-app.listen(3000, function () {
-    console.log('server is running on port 3000')
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
 })
