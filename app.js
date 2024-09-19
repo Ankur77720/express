e9bc59edf6af652196e2b407feb3693adba6e115
Ankur77720 <prajapatiankur8349@gmail.com>
2024-09-19 19:53:45 +0530

    morgan logger doe

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.set("view engine", "ejs");
+app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
